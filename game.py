0ce8846b727a80cf5a3e97c09d26cca875ba25c7
pryanikalenaknu <165229746+pryanikalenaknu@users.noreply.github.com>
2024-03-28 11:24:17 +0200

    config changed

--- game.py
+++ game.py
@@ -3,12 +3,14 @@ import sys
 import pygame
 from pygame import QUIT, KEYUP, K_ESCAPE
 
+from config import width_x, height_y
+
 
 class Game():
 
     def __init__(self):
         pygame.init()
-        pygame.display.set_mode((300 * 20, 400 * 20 + 40))
+        pygame.display.set_mode((width_x * 20, height_y * 20 + 40))
         pygame.display.set_caption('Minesweeper')
 
     def event(self, event):
