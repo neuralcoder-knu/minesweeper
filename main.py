2fbfb78def4a67fc16d0a9585272134e2db3f29a
neuralcoder-knu <118448583+neuralcoder-knu@users.noreply.github.com>
2024-03-28 11:30:20 +0200

    start game logic

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import pygame
+
+from config import tps
+from game import Game
+from location import StartLocation
+
 
 def main():
+    game = Game()
 
+    start_location = StartLocation(game)
+
+    game.location = start_location
+
+    clock = pygame.time.Clock()
+    while 1:
+        clock.tick(tps)
+        game.location.draw()
+        pygame.display.flip()
+        for event in pygame.event.get():
+            game.location.event(event)
+            game.event(event)
 
 if __name__ == "__main__":
     main()
