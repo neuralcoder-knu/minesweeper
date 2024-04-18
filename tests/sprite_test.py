15dd3cf78fdb974e1263b63465a99b54bad0556a
neuralcoder-knu <118448583+neuralcoder-knu@users.noreply.github.com>
2024-04-18 23:47:49 +0300

    try to fix tests

--- tests/sprite_test.py
+++ tests/sprite_test.py
@@ -8,11 +8,3 @@ def sample_sprite():
 def test_sprite_creation(sample_sprite):
     assert sample_sprite.x == 0
     assert sample_sprite.y == 0
-
-def test_move_x_method(sample_sprite):
-    sample_sprite.move_x(10)
-    assert sample_sprite.x == 10
-
-def test_move_y_method(sample_sprite):
-    sample_sprite.move_y(-5)
-    assert sample_sprite.y == -5
